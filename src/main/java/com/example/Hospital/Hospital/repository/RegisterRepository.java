23e9152e826b0a4cd20760cf665c4a9f36888b47
Matias-Marcelo <matiamarcelo756@gmail.com>
2025-05-23 16:04:16 +0200

    In #92: SQL Query Optimization, Fewer Statements

--- src/main/java/com/example/Hospital/Hospital/repository/RegisterRepository.java
+++ src/main/java/com/example/Hospital/Hospital/repository/RegisterRepository.java
@@ -16,11 +16,11 @@ public interface RegisterRepository extends CrudRepository<Register, Integer> {
 
 	Optional<Register> findByVitalSignId(int vitalSignId);
 
+	List<Register> findByPatientHistorialNumberAndDiagnosisIsNotNullOrderByDateDesc(int patientId);
+
 	@Query("SELECT r.patient.historialNumber AS id, r.observation AS obs FROM Register r "
 			+ "WHERE r.observation IS NOT NULL "
 			+ "AND r.date IN (SELECT MAX(r2.date) FROM Register r2 WHERE r2.observation IS NOT NULL GROUP BY r2.patient.historialNumber)")
 	List<Object[]> findLatestObservationsForPatients();
-
-	List<Register> findByPatientHistorialNumberAndDiagnosisIsNotNullOrderByDateDesc(int patientId);
 
 }
