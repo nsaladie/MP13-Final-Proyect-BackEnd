3469d8cb9acb39a550f7a58dc7339acb60d68eda
nsaladie <nsaladieg@gmail.com>
2025-05-27 00:48:15 +0200

    (REFACTOR): #98 Implement SQL search in repository method for targeted data retrieval

--- src/main/java/com/example/Hospital/Hospital/repository/RegisterRepository.java
+++ src/main/java/com/example/Hospital/Hospital/repository/RegisterRepository.java
@@ -1,26 +1,24 @@
 package com.example.Hospital.Hospital.repository;
 
-import java.util.Optional;
-import java.util.List;
+import java.util.*;
 
+import com.example.Hospital.Hospital.entity.*;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.repository.CrudRepository;
-
-import com.example.Hospital.Hospital.entity.Register;
 
 public interface RegisterRepository extends CrudRepository<Register, Integer> {
 
-	List<Register> findByPatientHistorialNumberAndVitalSignIsNotNullOrderByDateDesc(int patientId);
+    Optional<Register> findByVitalSignId(int vitalSignId);
 
-	Optional<Register> findTopByPatientHistorialNumberAndDiagnosisIsNotNullOrderByDateDesc(int historialNumber);
+    List<Register> findByPatientHistorialNumberAndDiagnosisIsNotNullOrderByDateDesc(int patientId);
 
-	Optional<Register> findByVitalSignId(int vitalSignId);
+    Optional<Register> findTopByPatientHistorialNumberAndDiagnosisIsNotNullOrderByDateDesc(int historialNumber);
 
-	List<Register> findByPatientHistorialNumberAndDiagnosisIsNotNullOrderByDateDesc(int patientId);
+    @Query("SELECT r.vitalSign FROM Register r WHERE r.patient.historialNumber = :patientId AND r.vitalSign IS NOT NULL ORDER BY r.date DESC")
+    List<VitalSign> findVitalSignsByPatientHistorialNumber(@Param("patientId") int patientId);
 
-	@Query("SELECT r.patient.historialNumber AS id, r.observation AS obs FROM Register r "
-			+ "WHERE r.observation IS NOT NULL "
-			+ "AND r.date IN (SELECT MAX(r2.date) FROM Register r2 WHERE r2.observation IS NOT NULL GROUP BY r2.patient.historialNumber)")
-	List<Object[]> findLatestObservationsForPatients();
+    @Query("SELECT r.patient.historialNumber AS id, r.observation AS obs FROM Register r " + "WHERE r.observation IS NOT NULL " + "AND r.date IN (SELECT MAX(r2.date) FROM Register r2 WHERE r2.observation IS NOT NULL GROUP BY r2.patient.historialNumber)")
+    List<Object[]> findLatestObservationsForPatients();
 
 }
